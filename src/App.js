3a615b058d7241a03cc5133b2b6ec6ddf06b0aa2
Gollos12255 <michiel.best@gmail.com>
2025-06-30 13:23:58 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>🚀 DOOH Planner Demo</h1>
+      <p>This is your live DOOH planner app. Your map and filters will go here.</p>
+    </div>
+  );
+}
+
+export default App;
